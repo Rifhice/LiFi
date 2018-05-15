2fd528c3d580dab7d42c2c69374e3ba32807718c
Assil <elyahyaouiassil@hotmail.com>
2018-05-15 13:38:55 +0200

    click add extra info

--- app/src/main/java/com/polytech/montpellier/lifiapp/DiscountSummary.java
+++ app/src/main/java/com/polytech/montpellier/lifiapp/DiscountSummary.java
@@ -3,6 +3,7 @@ package com.polytech.montpellier.lifiapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.oledcomm.soft.lifiapp.R;
@@ -13,6 +14,7 @@ import com.polytech.montpellier.lifiapp.Model.Discounts.Discount;
 import com.polytech.montpellier.lifiapp.Model.Discounts.PercentageDiscount;
 import com.polytech.montpellier.lifiapp.Model.Discounts.QuantityDiscount;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class DiscountSummary extends Activity {
@@ -37,6 +39,13 @@ public class DiscountSummary extends Activity {
             @Override
             public void onSuccess(Object object) {
                 final TextView brandTV = (TextView) findViewById(R.id.productBrand);
+                final TextView prodNameTV = (TextView) findViewById(R.id.productName);
+                final TextView dateFinTV = (TextView) findViewById(R.id.date_fin);
+                final ScrollView desciptionSV = (ScrollView) findViewById(R.id.descriptionScroll);
+                final TextView descriptionTV = (TextView) findViewById(R.id.descriptionView) ;
+
+
+
                 System.out.println("textView alalal" + object );
                 final TextView new_Price = (TextView) findViewById(R.id.newPrice);
                 final TextView old_Price = (TextView) findViewById(R.id.oldPrice);
@@ -50,30 +59,58 @@ public class DiscountSummary extends Activity {
                     PercentageDiscount discountper = new PercentageDiscount(discount.getProduct() , discount.getDateDebut(), discount.getDateFin(), discount.getDateCreation(),
                             ((PercentageDiscount) discount).getPercentage(), discount.getFidelity());
 
-                    String newString = Float.toString(discountper.newPrice());
-                    String oldString = Float.toString(discountper.oldPrice());
                     String brand = discountper.getProduct().getBrand();
+                    String prodName = discountper.getProduct().getName();
+                    String dateFin = new SimpleDateFormat( "yyyy-MM-dd").format(discountper.getDateFin());
+                    String desc = discountper.getProduct().getDescription();
+                    System.out.println("dateFin"+dateFin);
 
-                    System.out.println("disc new price"+newString);
-
-                    new_Price.setText(newString+R.string.currency);
-                    old_Price.setText(oldString+R.string.currency);
                     brandTV.setText(brand);
+                    prodNameTV.setText(prodName);
+                    dateFinTV.setText(dateFin);
+                    descriptionTV.setText(desc);
+
+                    Float newString = discountper.newPrice();
+                    Float oldString = discountper.oldPrice();
+
+                    double roundOffnew = Math.round(newString * 100.0) / 100.0;
+                    double roundOffold = Math.round(oldString * 100.0) / 100.0;
+
+                    System.out.println("disc new price"+Double.toString( roundOffnew) + "  "  +roundOffnew);
+
+
+                    new_Price.setText(Double.toString( roundOffnew)+" €");
+                    old_Price.setText(Double.toString( roundOffold) +" €");
+
 
                 }else if (discount instanceof QuantityDiscount){
 
                     QuantityDiscount discountqte = new QuantityDiscount(discount.getProduct() , discount.getDateDebut(), discount.getDateFin(), discount.getDateCreation(),
                             ((QuantityDiscount) discount).getBought(), ((QuantityDiscount) discount).getFree(), discount.getFidelity());
 
-                    String newString = Float.toString(discountqte.newPrice());
-                    String oldString = Float.toString(discountqte.oldPrice());
                     String brand = discountqte.getProduct().getBrand();
+                    String prodName = discountqte.getProduct().getName();
 
-                    System.out.println("disc newé price"+newString);
-                    
-                    new_Price.setText(newString+R.string.currency);
-                    old_Price.setText(oldString+R.string.currency);
                     brandTV.setText(brand);
+                    prodNameTV.setText(prodName);
+                    String dateFin = new SimpleDateFormat( "yyyy-MM-dd").format(discountqte.getDateFin());
+                    String desc = discountqte.getProduct().getDescription();
+
+                    Float newString = discountqte.newPrice();
+                    Float oldString = discountqte.oldPrice();
+
+                    double roundOffnew = Math.round(newString * 100.0) / 100.0;
+                    double roundOffold = Math.round(oldString * 100.0) / 100.0;
+
+                    System.out.println("disc new price"+Double.toString( roundOffnew) + "  "  +roundOffnew);
+
+
+                    new_Price.setText(Double.toString( roundOffnew) +" €");
+                    old_Price.setText(Double.toString( roundOffold) +" €");
+
+                    dateFinTV.setText(dateFin);
+                    descriptionTV.setText(desc);
+
                 }
             }
 
