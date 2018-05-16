cc17eead2396914d0ad8307c01f7b453f440a8d7
Unknown <chloe.dalger@etu.umontpellier.fr>
2018-05-16 09:55:58 +0200

    fix AdminProductView

--- app/src/main/java/com/polytech/montpellier/lifiapp/AdminProductView.java
+++ app/src/main/java/com/polytech/montpellier/lifiapp/AdminProductView.java
@@ -45,7 +45,7 @@ public class AdminProductView extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.productall_display, container, false);
+        return inflater.inflate(R.layout.lampall_display, container, false);
     }
 
     @Override
